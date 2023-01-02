21c334c5b41627bf79fdf1c746a02c56a9b29538
Fondao Sriworasa <fondao21011999@gmail.com>
2023-01-02 20:39:00 +0700

    update npm

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,85 @@
+import React from "react";
+import logo from '../../assets/logo.png';
+import "./Header.css";
+
+
+const Header = () => {
+    return (
+        <header className="header">
+            <nav className="nav container">
+            
+                    <a href="index.html" className="nav__logo">
+                        <img src={logo} alt="Logo" />
+                    </a>
+                
+                
+               <div className="nav_headmenu ">
+                {/* Home */}
+                <div className="nav__menu">
+                    <ul className="nav__list grid ">
+                        <li className="nav__item">
+                            <a href="#home" className="nav__link active-link ">
+                              <i className="uil uil-estate nav__icon "></i> Home
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+
+                {/* About */}
+                <div className="nav__menu">
+                    <ul className="nav__list grid">
+                        <li className="nav__item">
+                            <a href="#about" className="nav__link">
+                                <i className="uil uil-user nav__icon"></i> About
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+
+                {/* Skills */}
+                <div className="nav__menu">
+                    <ul className="nav__list grid">
+                        <li className="nav__item">
+                            <a href="#skills" className="nav__link">
+                                <i className="uil uil-file-alt nav__icon"></i> Skills
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+
+                {/* Portfolio */}
+                <div className="nav__menu">
+                    <ul className="nav__list grid">
+                        <li className="nav__item">
+                            <a href="#portfolio" className="nav__link">
+                                <i className="uil uil-scenery nav__icon"></i> Portfolio
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+
+                {/* Contact */}
+                <div className="nav__menu">
+                    <ul className="nav__list grid">
+                        <li className="nav__item">
+                            <a href="#contact" className="nav__link">
+                                <i className="uil uil-message nav__icon"></i> Contact
+                            </a>
+                        </li>
+                    </ul>
+
+                    {/* ICON CLOSE  X */}
+                    <i className="uil uil-times nav__close"></i>
+                </div>
+
+                <div className="nav__toggle">
+                    <i className="uil uil-apps"></i>
+                </div>
+               </div>
+                
+            </nav>
+        </header>
+    )
+}
+
+export default Header
