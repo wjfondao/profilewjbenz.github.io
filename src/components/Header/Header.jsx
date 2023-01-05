cf5d95890aeaefd4b60520e7a7fb4e03fbc868ee
Fondao Sriworasa <fondao21011999@gmail.com>
2023-01-05 21:21:44 +0700

    update header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,76 +7,61 @@ const Header = () => {
     return (
         <header className="header">
             <nav className="nav container">
-            
-                    <a href="index.html" className="nav__logo">
-                        <img src={logo} alt="Logo" />
-                    </a>
-                
-                
-               <div className="nav_headmenu ">
-                {/* Home */}
-                <div className="nav__menu">
-                    <ul className="nav__list grid ">
-                        <li className="nav__item">
-                            <a href="#home" className="nav__link active-link ">
-                              <i className="uil uil-estate nav__icon "></i> Home
-                            </a>
-                        </li>
-                    </ul>
+
+                <a href="index.html" className="nav__logo">
+                    <img src={logo} alt="Logo" />
+                </a>
+
+
+                <div className="nav_headmenu ">
+                    <div className="nav__menu">
+                        <ul className="nav__list grid ">
+
+                            {/* Home */}
+                            <li className="nav__item">
+                                <a href="#home" className="nav__link active-link ">
+                                    <i className="uil uil-estate nav__icon "></i> Home
+                                </a>
+                            </li>
+
+                            {/* About */}
+                            <li className="nav__item">
+                                <a href="#about" className="nav__link">
+                                    <i className="uil uil-user nav__icon"></i> About
+                                </a>
+                            </li>
+
+                            {/* Skills */}
+                            <li className="nav__item">
+                                <a href="#skills" className="nav__link">
+                                    <i className="uil uil-file-alt nav__icon"></i> Skills
+                                </a>
+                            </li>
+
+                            {/* Portfolio */}
+                            <li className="nav__item">
+                                <a href="#portfolio" className="nav__link">
+                                    <i className="uil uil-scenery nav__icon"></i> Portfolio
+                                </a>
+                            </li>
+
+                            {/* Contact */}
+                            <li className="nav__item">
+                                <a href="#contact" className="nav__link">
+                                    <i className="uil uil-message nav__icon"></i> Contact
+                                </a>
+                            </li>
+
+                            {/* ICON CLOSE  X */}
+                            <i className="uil uil-times nav__close"></i>
+                        </ul>
+                    </div>
+
+                    <div className="nav__toggle">
+                        <i className="uil uil-apps"></i>
+                    </div>
                 </div>
 
-                {/* About */}
-                <div className="nav__menu">
-                    <ul className="nav__list grid">
-                        <li className="nav__item">
-                            <a href="#about" className="nav__link">
-                                <i className="uil uil-user nav__icon"></i> About
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-
-                {/* Skills */}
-                <div className="nav__menu">
-                    <ul className="nav__list grid">
-                        <li className="nav__item">
-                            <a href="#skills" className="nav__link">
-                                <i className="uil uil-file-alt nav__icon"></i> Skills
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-
-                {/* Portfolio */}
-                <div className="nav__menu">
-                    <ul className="nav__list grid">
-                        <li className="nav__item">
-                            <a href="#portfolio" className="nav__link">
-                                <i className="uil uil-scenery nav__icon"></i> Portfolio
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-
-                {/* Contact */}
-                <div className="nav__menu">
-                    <ul className="nav__list grid">
-                        <li className="nav__item">
-                            <a href="#contact" className="nav__link">
-                                <i className="uil uil-message nav__icon"></i> Contact
-                            </a>
-                        </li>
-                    </ul>
-
-                    {/* ICON CLOSE  X */}
-                    <i className="uil uil-times nav__close"></i>
-                </div>
-
-                <div className="nav__toggle">
-                    <i className="uil uil-apps"></i>
-                </div>
-               </div>
-                
             </nav>
         </header>
     )
